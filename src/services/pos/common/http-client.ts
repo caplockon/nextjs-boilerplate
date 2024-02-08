189d4c49954ed2d7fa9241e05c1f9e35c2d033af
Phuc Nguyen <phucnt.tech@gmail.com>
2024-02-08 11:48:47 +0700

    Authentication

--- src/services/pos/common/http-client.ts
+++ src/services/pos/common/http-client.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
+import { getCookie } from 'cookies-next'
 
 export const http = axios.create({
-  baseURL: 'http://localhost:8081',
+  baseURL: process.env.NEXT_PUBLIC_POS_API_URL,
   method: 'get',
   headers: {
     'Access-Control-Allow-Origin': '*',
@@ -9,3 +10,16 @@ export const http = axios.create({
     'Access-Control-Allow-Credentials': 'true',
   },
 })
+
+http.interceptors.request.use(
+  (config) => {
+    const accessToken = getCookie('access_token')
+    if (accessToken) {
+      config.headers.setAuthorization(`Bearer ${accessToken}`)
+    }
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
